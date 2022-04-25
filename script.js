1a3a588acf3b64a77f16f05e37faab8e959c23b2
Bunchenko <andrew.by.one@gmail.com>
2022-04-25 22:26:59 +0300

    finish:finis project

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-const video= document.querySelector('.video');
+const video = document.querySelector('.video');
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(getMedia)
 
 async function getMedia() {
     const stream = await navigator.mediaDevices.getUserMedia({video: true});
     video.srcObject = stream;
 }
 
-getMedia()
+video.addEventListener('playing', () => {
+    //creating canvas
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    const displaySize = {width: video.width, height: video.height}
+
+    faceapi.matchDimensions(canvas, displaySize);
+
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video,
+        new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
+        .withFaceExpressions()
+
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections);
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    }, 100)
+})
